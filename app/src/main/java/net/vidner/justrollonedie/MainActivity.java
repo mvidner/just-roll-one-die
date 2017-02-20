2f8c97550b309fc786fcf26ce35a40480a716965
Martin Vidner <martin@vidner.net>
2017-02-20 14:19:04 +0100

    Roll a 7

--- app/src/main/java/net/vidner/justrollonedie/MainActivity.java
+++ app/src/main/java/net/vidner/justrollonedie/MainActivity.java
@@ -2,6 +2,8 @@ package net.vidner.justrollonedie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void roll(View view) {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText("7");
+    }
+
 }
